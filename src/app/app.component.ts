1196e1c303399ca1a51f380e3c8c3ddf7ab4a9de
abhishek hirwane <abhishek.hirwane@neewee.ai>
2022-05-07 08:29:23 +0530

    Reactive Forms2

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { FormBuilder,Validators } from '@angular/forms';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  constructor( private fb:FormBuilder){}
+
+  registrationForm=this.fb.group({
+    username:['',Validators.required],
+    password:[''],
+    confirmpassword:[''],
+  })
+
+
+
+}
